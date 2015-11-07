e13095dab1cab1f1b820f62f75fd900d5ad65317
pratik <pratik2709@gmail.com>
2015-11-07 20:24:05 +0530

    Created library for transform functions

--- main.js
+++ main.js
@@ -4,20 +4,68 @@ var physicsEngine = (function (run) {
     var context = canvas.getContext('2d');
     var rectangle = {
 
-        x: 500,
-        y: 500,
-        w: 200,
-        h: 200
+        x: 10,
+        y: 900,
+        w: 50,
+        h: 50
     };
 
     var rectangle2 = {
-        x: 500,
-        y: 500,
-        w: 200,
-        h: 200
+        x: 50,
+        y: 900,
+        w: 50,
+        h: 50
     };
-    //context.rotate(-45 * (3.14) / 180);
-    run.draw.drawPolygon(context, rectangle.x, rectangle.y, rectangle.w, rectangle.h, '"#FE8E9D"');
-    //context.rotate(45 * (3.14) / 180);
+
+    context.rotate(-45 * (3.14) / 180); //entire context has been rotated
+    run.draw.drawPolygon(context,rectangle.x, rectangle.y, rectangle.w, rectangle.h, '"#FE8E9D"');
+
+    context.rotate(45 * (3.14) / 180); //undo rotation
+
+
+    var t = new run.transform_library.transform();
+    t.rotate(-45 * (3.14) / 180);
+    var d4 = t.transformPoint(rectangle.x, rectangle.y);
+    var d1 = t.transformPoint(rectangle.x + rectangle.w, rectangle.y);
+    var d2 = t.transformPoint(rectangle.x + rectangle.w, rectangle.y + rectangle.h);
+    var d3 = t.transformPoint(rectangle.x, rectangle.y + rectangle.h);
+    var d0 = t.transformPoint(rectangle.x + (rectangle.w) / 2, rectangle.y + (rectangle.h) / 2);
+
+//console.log(pt);
+    console.log(run.vectorlib);
+    var dot14 = new run.vectorlib.vector(d4[0], d4[1]);
+    var dot11 = new run.vectorlib.vector(d1[0], d1[1]);
+    var dot12 = new run.vectorlib.vector(d2[0], d2[1]);
+    var dot13 = new run.vectorlib.vector(d3[0], d3[1]);
+    var dot10 = new run.vectorlib.vector(d0[0], d0[1]);
+
+    //drawLine(dot11,dot12);
+    //drawLine(dot12,dot13);
+    //drawLine(dot13,dot14);
+    //drawLine(dot14,dot11);
+
+
+    context.rotate(-45 * (3.14) / 180);
+    run.draw.drawPolygon(context,rectangle2.x, rectangle2.y, rectangle2.w, rectangle2.h, "#FE8E9D");
+
+    context.rotate(45 * (3.14) / 180); //undo rotation
+
+    var d24 = t.transformPoint(rectangle2.x, rectangle2.y);
+    var d21 = t.transformPoint(rectangle2.x + rectangle2.w, rectangle2.y);
+    var d22 = t.transformPoint(rectangle2.x + rectangle2.w, rectangle2.y + rectangle2.h);
+    var d23 = t.transformPoint(rectangle2.x, rectangle2.y + rectangle2.h);
+    var d20 = t.transformPoint(rectangle2.x + (rectangle2.w) / 2, rectangle2.y + (rectangle2.h) / 2);
+
+    //console.log(pt);
+    var dot24 = new run.vectorlib.vector(d24[0], d24[1]);
+    var dot21 = new run.vectorlib.vector(d21[0], d21[1]);
+    var dot22 = new run.vectorlib.vector(d22[0], d22[1]);
+    var dot23 = new run.vectorlib.vector(d23[0], d23[1]);
+    var dot20 = new run.vectorlib.vector(d20[0], d20[1]);
+    //
+    //    drawLine(dot21,dot22);
+    //    drawLine(dot22,dot23);
+    //    drawLine(dot23,dot24);
+    //    drawLine(dot24,dot21);
 
 })(physicsEngine || {});
---
+++ transform_library1.js
@@ -0,0 +1,42 @@
+var physicsEngine = (function (run) {
+
+    run.transform_library = (function () {
+        var Transform = function() {
+            this.reset();
+        };
+
+        Transform.prototype.reset = function () {
+            this.m = [1, 0, 0, 1, 0, 0];
+        };
+
+
+        Transform.prototype.rotate = function (rad) {
+            var c = Math.cos(rad);
+            var s = Math.sin(rad);
+            var m11 = this.m[0] * c + this.m[2] * s;
+            var m12 = this.m[1] * c + this.m[3] * s;
+            var m21 = this.m[0] * -s + this.m[2] * c;
+            var m22 = this.m[1] * -s + this.m[3] * c;
+            this.m[0] = m11;
+            this.m[1] = m12;
+            this.m[2] = m21;
+            this.m[3] = m22;
+        };
+
+        Transform.prototype.transformPoint = function (px, py) {
+            var x = px;
+            var y = py;
+            px = x * this.m[0] + y * this.m[2] + this.m[4];
+            py = x * this.m[1] + y * this.m[3] + this.m[5];
+            return [px, py];
+        };
+
+        return {
+            transform: Transform
+        }
+
+    })();
+
+    return run
+
+})(physicsEngine || {});
